c0a083baa035096ebef98781e9716d4dc3a165f9
Hieronimus Fredy Morgan <morganhero35@gmail.com>
2020-08-27 09:37:16 +0700

    wah

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,11 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid">
-
+        <div class="d-flex align-items-center
+                            justify-content-between small">
+            <div class="text-muted">Copyright &copy; Atanasius Ivannoel Rio Aji, Hieronimus Fredy Morgan, Yudistira
+                Prama Putra, Edrick Hernando</div>
+        </div>
+    </div>
     </div>
 </footer>
 </div>
@@ -10,7 +15,7 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" crossorigin="anonymous">
 </script>
-<script src="<?= base_url('assets/') ?>js/scripts.js"></script> 
+<script src="<?= base_url('assets/') ?>js/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
 <script src="assets/demo/chart-area-demo.js"></script>
 <script src="assets/demo/chart-bar-demo.js"></script>
